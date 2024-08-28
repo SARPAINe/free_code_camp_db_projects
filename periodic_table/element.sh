af05f5f19db6c794783cd8db62979dd0ddac30cd
Shaharin Ahmed <ahmedshaharin7589@gmail.com>
2024-08-28 15:08:10 +0600

    refactor: refactor periodic table element.sh file

--- periodic_table/element.sh
+++ periodic_table/element.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
+
+# Define PSQL command setup
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
-MAIN_PROGRAM() {
-  if [[ -z $1 ]]
-  then
+# Function to fetch data from the database
+fetch_data() {
+  local query=$1
+  local result=$(echo $($PSQL "$query") | sed 's/ //g')
+  echo "$result"
+}
+
+# Main entry point for script
+main() {
+  local element=$1
+  if [[ -z $element ]]; then
     echo "Please provide an element as an argument."
   else
-    PRINT_ELEMENT $1
+    get_element_details "$element"
   fi
 }
 
-PRINT_ELEMENT() {
-  INPUT=$1
-  if [[ ! $INPUT =~ ^[0-9]+$ ]]
-  then
-    ATOMIC_NUMBER=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE symbol='$INPUT' OR name='$INPUT';") | sed 's/ //g')
+# Function to find element and print its details
+get_element_details() {
+  local input=$1
+  local search_column="atomic_number"
+
+  # Check if the input is a number or text for atomic_number or symbol/name
+  if [[ ! $input =~ ^[0-9]+$ ]]; then
+    search_column="symbol='$input' OR name='$input'"
   else
-    ATOMIC_NUMBER=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$INPUT;") | sed 's/ //g')
+    search_column="atomic_number='$input'"
   fi
-  
-  if [[ -z $ATOMIC_NUMBER ]]
-  then
+
+  local atomic_number=$(fetch_data "SELECT atomic_number FROM elements WHERE $search_column;")
+
+  if [[ -z $atomic_number ]]; then
     echo "I could not find that element in the database."
   else
-    TYPE_ID=$(echo $($PSQL "SELECT type_id FROM properties WHERE atomic_number=$ATOMIC_NUMBER;") | sed 's/ //g')
-    NAME=$(echo $($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER;") | sed 's/ //g')
-    SYMBOL=$(echo $($PSQL "SELECT symbol FROM elements WHERE atomic_number=$ATOMIC_NUMBER;") | sed 's/ //g')
-    ATOMIC_MASS=$(echo $($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NUMBER;") | sed 's/ //g')
-    MELTING_POINT_CELSIUS=$(echo $($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER;") | sed 's/ //g')
-    BOILING_POINT_CELSIUS=$(echo $($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER;") | sed 's/ //g')
-    TYPE=$(echo $($PSQL "SELECT type FROM elements LEFT JOIN properties USING(atomic_number) LEFT JOIN types USING(type_id) WHERE atomic_number=$ATOMIC_NUMBER;") | sed 's/ //g')
+    # Collect all necessary properties
+    local name=$(fetch_data "SELECT name FROM elements WHERE atomic_number=$atomic_number;")
+    local symbol=$(fetch_data "SELECT symbol FROM elements WHERE atomic_number=$atomic_number;")
+    local atomic_mass=$(fetch_data "SELECT atomic_mass FROM properties WHERE atomic_number=$atomic_number;")
+    local melting_point_celsius=$(fetch_data "SELECT melting_point_celsius FROM properties WHERE atomic_number=$atomic_number;")
+    local boiling_point_celsius=$(fetch_data "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$atomic_number;")
+    local type=$(fetch_data "SELECT type FROM types JOIN properties ON types.type_id=properties.type_id WHERE atomic_number=$atomic_number;")
 
-    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+    # Output element information
+    echo "The element with atomic number $atomic_number is $name ($symbol). It's a $type, with a mass of $atomic_mass amu. $name has a melting point of $melting_point_celsius celsius and a boiling point of $boiling_point_celsius celsius."
   fi
 }
 
-MAIN_PROGRAM $1
+# Run the main program passing all arguments
+main $1
